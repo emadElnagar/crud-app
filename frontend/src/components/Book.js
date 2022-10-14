8aad536d7b60839f11b49941900d957451cd01ca
unknown <emad.m.alnagar@gmail.com>
2022-10-14 05:24:08 +0200

    Delete book

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -3,6 +3,8 @@ import { DeleteButton, ListItem, UpdateButton, CheckButton, UpdateForm, Input, S
 import { MdDelete, MdModeEdit } from "react-icons/md";
 import { GiCheckMark } from "react-icons/gi";
 import axios from "axios";
+import Swal from 'sweetalert2'
+
 
 function Book (props) {
   const { book } = props;
@@ -14,10 +16,31 @@ function Book (props) {
     setIsUpdate(false);
   }
   // Update book
-  const handleUpdate = async (id) =>{
+  const handleUpdate = async (id) => {
     await axios.put(`http://127.0.0.1:5000/books/${id}/update`, {
       title,
     });
+  }
+  // Delete book
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.delete(`http://127.0.0.1:5000/books/${id}/delete`);
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
   }
   return (
     <Fragment>
@@ -26,7 +49,7 @@ function Book (props) {
         <div>
           <CheckButton title="Read"><GiCheckMark /></CheckButton>
           <UpdateButton title="Update" onClick={() => setIsUpdate(true)}><MdModeEdit /></UpdateButton>
-          <DeleteButton title="Delete"><MdDelete /></DeleteButton>
+          <DeleteButton onClick={() => handleDelete(book._id)} title="Delete"><MdDelete /></DeleteButton>
         </div>
       </ListItem>
       <div>
